17a9ff1c28518ea1699bd944282d348c266726fe
kimberlin alfonzo <kim99alfonzo@gmail.com>
2025-04-05 11:45:18 -0400

    create_function_modf

--- post/views.py
+++ post/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse_lazy
 class PostListView (ListView):
     template_name = 'post_list.html'
     model = Articulo
-    context_object_name = 'lista_objects'  # Nombre del contexto para la lista de artículos
+    context_object_name = 'lista'  # Nombre del contexto para la lista de artículos
     fields = ['titulo', 'descripcion', 'imagen', 'author']  # Campos del formulario
 
 
