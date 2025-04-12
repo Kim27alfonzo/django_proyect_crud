962841bafd70c9dfc196edff4a44f413873d6802
kimberlin alfonzo <kim99alfonzo@gmail.com>
2025-04-12 11:12:15 -0400

    add feature delete

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from post.views import PostListView, PostCreateView, PostReadView, PostUpdateView
+from post.views import PostListView, PostCreateView, PostReadView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
 
     path('', PostListView.as_view(), name='post_list'),  # URL para la lista de artículos
-    path('articuo/create/', PostCreateView.as_view(), name='post_create'),  # URL para crear un nuevo artículo
+    path('articulo/create/', PostCreateView.as_view(), name='post_create'),  # URL para crear un nuevo artículo
     path('articulo/read/<int:pk>', PostReadView.as_view(), name='post_read'),  # URL para leer un artículo específico
     path('articulo/read/<int:pk>/update',PostUpdateView.as_view(), name='post_update'),  # URL para actualizar un artículo específico
-    
+    path('articulo/read/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),  # URL para eliminar un artículo específico
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Articulo
 from django.views.generic import ListView
 from django.views.generic import DetailView
@@ -32,4 +32,10 @@ class PostUpdateView (UpdateView):
     model = Articulo
     fields = ['titulo', 'descripcion', 'imagen']  # Campos del formulario
     context_object_name = 'update'  # Nombre del contexto para el formulario
+    success_url = reverse_lazy("post_list")  # URL de redirección después de crear el artículo
+
+class PostDeleteView (DeleteView):
+    template_name = 'post_delete.html'
+    model = Articulo
+    context_object_name = 'delete'  # Nombre del contexto para el formulario
     success_url = reverse_lazy("post_list")  # URL de redirección después de crear el artículo
