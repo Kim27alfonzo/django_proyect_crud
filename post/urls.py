132a9b721cbf55a30452ce00e836a5d971a7d393
kimberlin alfonzo <kim99alfonzo@gmail.com>
2025-04-12 09:05:47 -0400

    crud

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from post.views import PostListView, PostCreateView
+from post.views import PostListView, PostCreateView, PostReadView
 
 urlpatterns = [
 
     path('', PostListView.as_view(), name='post_list'),  # URL para la lista de artículos
     path('create/', PostCreateView.as_view(), name='post_create'),  # URL para crear un nuevo artículo
+    path('read/<int:pk>', PostReadView.as_view(), name='post_read'),  # URL para leer un artículo específico
 ]
--- post/views.py
+++ post/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Articulo
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -9,7 +10,7 @@ class PostListView (ListView):
     template_name = 'post_list.html'
     model = Articulo
     context_object_name = 'lista'  # Nombre del contexto para la lista de artículos
-    fields = ['titulo', 'descripcion', 'imagen', 'author']  # Campos del formulario
+    
 
 
 class PostCreateView(CreateView):
@@ -17,4 +18,11 @@ class PostCreateView(CreateView):
     model = Articulo
     fields = ['titulo', 'descripcion', 'imagen', 'author']  # Campos del formulario
     context_object_name = 'create' # Nombre del contexto para el formulario
+    success_url = reverse_lazy("post_list")  # URL de redirección después de crear el artículo
+
+class PostReadView (DetailView):
+    template_name = 'post_read.html'
+    model = Articulo
+    context_object_name = 'read'  # Nombre del contexto para el artículo
+    fields = ['titulo', 'descripcion', 'imagen', 'author']  # Campos del formulario
     success_url = reverse_lazy("post_list")  # URL de redirección después de crear el artículo
